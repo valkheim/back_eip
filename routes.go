d77a5fea3244e38957050a57b5fd941c7fea3018
Charles Paulet <charles.paulet@epitech.eu>
2017-11-05 23:17:00 +0100

    Add default headers for each handler

--- routes.go
+++ routes.go
@@ -10,10 +10,10 @@ func addDefaultHeaders(f http.HandlerFunc) http.HandlerFunc {
 }
 
 func (app *App) initializeRoutes() {
-  app.Router.HandleFunc("/client/{id:[0-9]+}", getClientHandler).Methods("GET")
-  app.Router.HandleFunc("/client/{id:[0-9]+}", deleteClientHandler).Methods("DELETE")
+  app.Router.HandleFunc("/client/{id:[0-9]+}", addDefaultHeaders(getClientHandler)).Methods("GET")
+  app.Router.HandleFunc("/client/{id:[0-9]+}", addDefaultHeaders(deleteClientHandler)).Methods("DELETE")
   app.Router.HandleFunc("/client", addDefaultHeaders(createHandler(createClient, &Client{}))).Methods("POST")
-  app.Router.HandleFunc("/partner", createHandler(createPartner, &Partner{})).Methods("POST")
+  app.Router.HandleFunc("/partner", addDefaultHeaders(createHandler(createPartner, &Partner{}))).Methods("POST")
 
   app.Router.HandleFunc("/debugssl", func(w http.ResponseWriter, req *http.Request) {
     w.Header().Add("Strict-Transport-Security", "max-age=63072000; includeSubDomains")
