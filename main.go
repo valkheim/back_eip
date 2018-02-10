c780b92070f3ff73c0eaaba92c89cac63ea46a29
Charles Paulet <charles.paulet@epitech.eu>
2018-02-10 11:59:41 +0100

    Listen to proper docker container host

--- main.go
+++ main.go
@@ -1,12 +1,6 @@
 package main
 
-//import (
-//  "fmt"
-//  "./store"
-//)
-
 func main() {
-  //fmt.Print(store.GetAllKeys(), "\n")
-  a := Api{Address:"127.0.0.1:443", Timeout:15, Router:nil}
+  a := Api{Address:"api:443", Timeout:15, Router:nil}
   a.Run()
 }
